a640701e3d93e25dad47f9f43adcaad50e94b98f
Maskani Filali Mohamed <maskani.mohamed@gmail.com>
2017-03-08 15:13:29 +0100

    add main to quick_sort

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,4 +1,3 @@
-import copy as cp
 import numpy as np
 
 """
@@ -26,6 +25,7 @@ def quick_sort(my_list):
     if len(my_list) <= 1:
         return my_list
     else:
+        #TODO : Add random selection of pivot ?
         pivot = my_list[0]
         left, right = [], []
         for x in my_list[1:]:
@@ -34,3 +34,17 @@ def quick_sort(my_list):
             else:
                 right.append(x)
         return quick_sort(left) + [pivot] + quick_sort(right)
+
+if __name__ == '__main__':
+
+    if 0:
+        l = np.random.randint(1, 10, 10)
+        print("List to sort : {}".format(l))
+        sorted_l = quick_sort(l)
+        print( "List sorted : {}".format(sorted_l))
+
+    if 1:
+        l = [1, 4, 5, 5, 5, 8, 8, 10, 0, -1, -2, 10, 9]
+        print("List to sort : {}".format(l))
+        sorted_l = quick_sort(l)
+        print("List sorted : {}".format(sorted_l))
